97cc7bd40e28fdd245be5e72a01c268ee6f4c346
wangyang <1135071742@qq.com>
2024-12-15 13:38:10 +0000

    测试dataloader

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from train_test import ARCDetector
 import numpy as np
 from torch_geometric.nn import Node2Vec, GCN
 
+from dataloader import DataLoader
+
 
 def set_seed(seed):
     random.seed(seed)
@@ -15,6 +17,24 @@ def set_seed(seed):
         torch.backends.cudnn.deterministic = True
 
 
+def test_dataloader(data: Data):
+    loader = DataLoader(
+        data,
+        num_neighbors=[10] * 2,
+        walk_length=20,
+        context_size=10,
+        walks_per_node=10,
+        p=1.0,
+        q=1.0,
+        batch_size=128)
+    # TODO: 注意sample_data会有很多节点，但是只需要batch_size的长度，因此最后的embedding只取最后的batch_size的切片
+    # TODO： 具体参考DOMINANT pygod实现
+    for sample_data, pos_rw, neg_rw in loader:
+        print(sample_data)
+        print(pos_rw)
+        print(neg_rw)
+
+
 warnings.filterwarnings("ignore")
 parser = argparse.ArgumentParser()
 parser.add_argument('--trials', type=int, default=5)
@@ -23,7 +43,8 @@ parser.add_argument('--shot', type=int, default=10)
 parser.add_argument('--json_dir', type=str, default='./params')
 args = parser.parse_known_args()[0]
 
-datasets_test = ['cora', 'citeseer', 'ACM', 'BlogCatalog', 'Facebook', 'weibo', 'Reddit', 'Amazon', 'cs', 'photo', 'tolokers']
+datasets_test = ['cora', 'citeseer', 'ACM', 'BlogCatalog',
+                 'Facebook', 'weibo', 'Reddit', 'Amazon', 'cs', 'photo', 'tolokers']
 datasets_train = ['pubmed', 'Flickr', 'questions', 'YelpChi']
 
 model = args.model
@@ -41,20 +62,21 @@ dims = 64
 data_train = [Dataset(dims, name) for name in datasets_train]
 data_test = [Dataset(dims, name) for name in datasets_test]  # CPU
 model_config = read_json(model, args.shot, args.json_dir)
+test_dataloader(data_test[0].graph)
 
 if model_config is None:
     model_config = {
-          "model": "ARC",
-          "lr": 1e-5,
-          "drop_rate": 0,
-          "h_feats": 1024,
-          "num_prompt": 10,
-          "num_hops": 2,
-          "weight_decay": 5e-5,
-          "in_feats": 64,
-          "num_layers": 4,
-          "activation": "ELU"
-        }
+        "model": "ARC",
+        "lr": 1e-5,
+        "drop_rate": 0,
+        "h_feats": 1024,
+        "num_prompt": 10,
+        "num_hops": 2,
+        "weight_decay": 5e-5,
+        "in_feats": 64,
+        "num_layers": 4,
+        "activation": "ELU"
+    }
     print('use default model config')
 else:
     print('use saved best model config')
@@ -106,4 +128,3 @@ for test_data_name in auc_mean_dict:
         pre_std_dict[test_data_name])
     print('-' * 50 + test_data_name + '-' * 50)
     print('str_result', str_result)
-
